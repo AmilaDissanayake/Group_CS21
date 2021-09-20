4f5b21744f7a10265a279dd2c13b90b8eaaaf11e
Navod-Shehan <2019cs196@stu.ucsc.lk>
2021-09-20 13:36:40 +0530

    fvdv

--- calculator/bmi.js
+++ calculator/bmi.js
@@ -1,22 +1,22 @@
 let gender = 'male';
 height = 50;
 
-document.querySelector('.calculate .gender .male').onclick = function () {
-    gender = 'male';
-    this.classList.add('active');
-    document.querySelector('.calculate . gender .female').className = 'female';
-}
+// document.querySelector('.calculate .gender .male').onclick = function () {
+//     gender = 'male';
+//     this.classList.add('active');
+//     document.querySelector('.calculate . gender .female').className = 'female';
+// }
 
-document.querySelector('.calculate .gender .male').onclick = function () {
-    gender = 'female';
-    this.classList.add('active');
-    document.querySelector('.calculate . gender .male').className = 'male';
-}
+// document.querySelector('.calculate .gender .male').onclick = function () {
+//     gender = 'female';
+//     this.classList.add('active');
+//     document.querySelector('.calculate . gender .male').className = 'male';
+// }
 
-document.querySelector('.calculate .height input').onchange = function () {
-    height = parseInt(this.value);
-    document.querySelector('.calculate .height .val span').innerText = height;
-}
+// document.querySelector('.calculate .height input').onchange = function () {
+//     height = parseInt(this.value);
+//     document.querySelector('.calculate .height .val span').innerText = height;
+// }
 
 document.querySelector('.calculate .height .val i.add').onclick = function () {
     height += 1;
@@ -32,18 +32,18 @@ document.querySelector('.calculate .height .val i.sub').onclick = function () {
     document.querySelector('.calculate .height input').value = height;
 }
 
-document.querySelector('.calculate .height .val i.add').onclick = function () {
-    Age += 1;
-    Age = (Age > 220) ? 220 : Age;
-    document.querySelector('.calculate .height .val span').innerText = Age;
-    document.querySelector('.calculate .height input').value = Age;
+document.querySelector('.calculate .age .val i.add').onclick = function () {
+    age += 1;
+    age = (age > 70) ? 70 : age;
+    document.querySelector('.calculate .age .val span').innerText = age;
+    document.querySelector('.calculate .age input').value = age;
 }
 
-document.querySelector('.calculate .height .val i.sub').onclick = function () {
+document.querySelector('.calculate .age .val i.sub').onclick = function () {
     height -= 1;
     height = (height < 50) ? 50 : height;
-    document.querySelector('.calculate .height .val span').innerText = height;
-    document.querySelector('.calculate .height input').value = height;
+    document.querySelector('.calculate .age .val span').innerText = age;
+    document.querySelector('.calculate .age input').value = age;
 }
 
 document.querySelector('.calculate .weight input').onchange = function () {
@@ -74,23 +74,7 @@ document.querySelector('.calculate .calc').onclick = function () {
     document.querySelector('.calculate .height input').value = height;
     document.querySelector('.calculate .height .val span').innerText = height;
 
-    document.querySelector('.result .bmi .val').innerText = bmi;
 
-    if (bmi < 18.5) {
-        document.querySelector('.result .text').innerText = 'YOU ARE UNDERWEIGHT!';
-        document.querySelector('.result .bmi .val').style.color = '#3f51b5';
-        document.querySelector('.result .text').style.color = '#3f51b5';
-    }
-    else if (bmi > 18.5 && bmi < 25) {
-        document.querySelector('.result .text').innerText = 'YOU HAVE A HEALTHY WEIGHT!';
-        document.querySelector('.result .bmi .val').style.color = '#0f4';
-        document.querySelector('.result .text').style.color = '#0f4';
-    }
-    else {
-        document.querySelector('.result .text').innerText = 'YOU ARE OVERWEIGHT!';
-        document.querySelector('.result .bmi .val').style.color = '#ffc107';
-        document.querySelector('.result .text').style.color = '#ffc107';
-    }
 
     document.querySelector('.calculate').style.display = 'none';
     document.querySelector('.result').style.display = 'flex';
