7b28d61a467c9ca0e8f0b0cc1204d350e2b4c140
Bimsara Kulasekara <bkdeveloping@gmail.com>
2021-09-23 17:56:49 +0530

    trainers

--- root/trainers.php
+++ root/trainers.php
@@ -11,6 +11,7 @@
 
 <body>
 
+    <!-- <div class="vk"> -->
 
     <?php
 
@@ -22,41 +23,43 @@
 
     while ($row = mysqli_fetch_assoc($select_query)) {
         $f_name = $row['f_name'];
+        $l_name = $row['l_name'];
+        $image = $row['image'];
         // $post_author = $row['post_author'];
         // $post_date = $row['post_date'];
         // $post_content = $row['post_content'];
     ?>
 
-        <div class="vk">
 
-            <div class="card">
-                <div class="card-image"></div>
-                <div class="card-text">
-                    <span class="date">4 days ago</span>
-                    <h2><?php echo $f_name ?></h2>
-                    <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
+
+        <div class="card">
+            <div class="card-image"><img src="media/trainers/<?php echo $image ?>"></div>
+            <div class="card-text">
+                <span class="date">4 days ago</span>
+                <h2><?php echo $f_name . " " . $l_name ?></h2>
+                <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
+            </div>
+            <div class="card-stats">
+                <div class="stat">
+                    <div class="value">4<sup>m</sup></div>
+                    <div class="type">read</div>
                 </div>
-                <div class="card-stats">
-                    <div class="stat">
-                        <div class="value">4<sup>m</sup></div>
-                        <div class="type">read</div>
-                    </div>
-                    <div class="stat border">
-                        <div class="value">5123</div>
-                        <div class="type">views</div>
-                    </div>
-                    <div class="stat">
-                        <div class="value">32</div>
-                        <div class="type">comments</div>
-                    </div>
+                <div class="stat border">
+                    <div class="value">5123</div>
+                    <div class="type">views</div>
+                </div>
+                <div class="stat">
+                    <div class="value">32</div>
+                    <div class="type">comments</div>
                 </div>
             </div>
         </div>
+
     <?php
     }
     ?>
 
-
+    <!-- </div> -->
 
 </body>
 
