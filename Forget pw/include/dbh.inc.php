fe994ede7be918753255532f8c6d20df64eb7f27
AmilaDissanayake <2019cs039@stu.ucsc.lk>
2021-09-25 12:08:14 +0530

    forget pw added

---
+++ Forget pw/include/dbh.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+$dBServername = "localhost";
+$dBUsername = "root";
+$dBPassword = "";
+$dBName = "test";
+
+$conn = mysqli_connect($dBServername,$dBUsername, $dBPassword, $dBName);
+
+if(!$conn){
+    die("Connection failed :". mysqli_connect_error());
+}
+
+?>
