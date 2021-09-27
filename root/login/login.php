edadda5f23cefb253edf92ed4fb577e39896e5ad
Bimsara Kulasekara <bkdeveloping@gmail.com>
2021-09-28 00:15:56 +0530

    sdvdfg

--- root/login/login.php
+++ root/login/login.php
@@ -1,3 +1,5 @@
+<?php session_start(); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,7 +14,9 @@
     <link rel="stylesheet" href="login.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+
 </head>
 
 <body>
@@ -25,40 +29,83 @@
             <h5 class="head5">BUILD PERFECT BODY <br> SHAPE FOR <span>GOOD</span> AND <br><span>HEALTHY</span> LIFE.
             </h5>
             </p>
-                <div class="homeIcon">
-                    <a href="../home/index.html" class="fas fa-backward"></a>
-                    <a href="../home/index.html">Back to home</a>
-                </div>
+            <div class="homeIcon">
+                <a href="index.html" class="fas fa-backward"></a>
+                <a href="index.html">Back to home</a>
+            </div>
         </div>
         <div class="login">
             <div class="l-form">
-                <form action="" class="form" id="form">
-                    <h1 class="form__title">LOGIN</h1>
-
+                <form action="" class="form" id="form" method="POST">
+                    <h1 class="form__title">LOGIN<span id="Mem"></span></h1>
+                    <!-- <span id="Mem">(MEMBER)</span><span id = "Tr">(TRAINER)</span> -->
                     <div class="form__div">
-                        <input type="text" class="form__input" id="username" placeholder=" ">
-                        <label for="" class="form__label">Username or Email</label>
+                        <div class="select__div">
+                            <label>
+                                <select class="form_input" id="type" onChange="modechange()" required>
+                                    <option value="" disabled selected> Select Login Type </option>
+                                    <option value="1">Trainer</option>
+                                    <option value="2">Member</option>
+                                </select>
+                            </label>
+                        </div>
                         <i class="fa fa-check"></i>
-                        <!-- <i class="fas fa-check-circle"></i> -->
                         <i class="fas fa-exclamation-triangle"></i>
-                        <!-- <i class="fas fa-exclamation"></i> -->
-                        <!-- <i class="fas fa-exclamation-circle"></i> -->
                         <small>Error message</small>
                     </div>
 
                     <div class="form__div">
-                        <input type="password" class="form__input" id="password" placeholder=" " id="password" name="password">
+                        <input type="text" class="form__input" id="username" placeholder=" " name="username">
+                        <label for="" class="form__label">Username or Email</label>
+                        <i class="fa fa-check"></i>
+                        <i class="fas fa-exclamation-triangle"></i>
+                        <small>Error message</small>
+                    </div>
+
+                    <div class="form__div">
+                        <input type="password" class="form__input" id="password" placeholder=" " name="password">
 
                         <label for="" class="form__label">Password</label>
                         <i class="fa fa-check"></i>
-                        <!-- <i class="fas fa-check-circle"></i> -->
                         <i class="fas fa-exclamation-triangle"></i>
-                        <!-- <i class="fas fa-exclamation"></i> -->
-                        <!-- <i class="fas fa-exclamation-circle"></i> -->
                         <small>Error message</small>
                     </div>
 
-                    <div class="buttondiv"><input type="submit" class="form__button" value="Login"></div>
+                    <div class="buttondiv"><input type="submit" class="form__button" value="Login" name="submit"></div>
+
+                    <!-- <button class="err">Show Alert</button> -->
+                    <div class="alert hide">
+                        <!-- <span class="fas fa-exclamation-circle"></span> -->
+                        <span class="msg">Username or password is Incorrect!</span>
+                        <div class="close-btn">
+                            <span class="fas fa-times"></span>
+                        </div>
+                    </div>
+
+                    <script>
+                        function nn() {
+                            $('.alert').addClass("show");
+                            $('.alert').removeClass("hide");
+                            $('.alert').addClass("showAlert");
+                            setTimeout(function bb() {
+                                $('.alert').removeClass("show");
+                                $('.alert').addClass("hide");
+                            }, 500000);
+                        };
+                        $('.close-btn').click(function ss() {
+                            $('.alert').removeClass("show");
+                            $('.alert').addClass("hide");
+                        });
+                    </script>
+
+
+                    <?php
+                    if (isset($_SESSION['error'])) {
+                        $error = $_SESSION['error'];
+                        echo '<script type="text/javascript">nn();</script>';
+                    }
+                    ?>
+
                     <div class="remember">
                         <label><input type="checkbox" name=""> Remember me</label>
                         <span class="checkmark"></span>
@@ -79,16 +126,10 @@
 
                     </div>
                 </form>
-
-                <?php
-                if(isset($_GET["newpwd"])){
-                    if($_GET["newpwd"] == "passwordUpdated"){
-                        echo "Your password has been reset";
-                    }
-                }
-                
-                ?>
             </div>
+
+
+
 
         </div>
     </section>
@@ -97,3 +138,7 @@
 </body>
 
 </html>
+
+<?php
+unset($_SESSION['error']);
+?>
