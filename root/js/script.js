ed4254651c55b9787f7bf10cffcca4e73ff97e15
= <pamodhamahagamage98@gmail.com>
2021-09-28 23:11:07 +0530

    DropDown

--- root/js/script.js
+++ root/js/script.js
@@ -1,20 +1,34 @@
 
-var btn = document.getElementById("myBtn").addEventListener("click", function(e){
+var btn = document.getElementById("myBtn");
+
+btn.addEventListener("click", function(e){
     choose();
 })
 
 function choose(){
+    const va = btn.parentElement.value;
 
-  var drop = document.getElementsByClassName("dropdown-menu information-grid");  
-  if(drop.display === "none"){
-        drop.display = "block";
-        alert("dddp");
-  }else if(drop.display === "block"){
-        drop.display = "none";
-  }else{
-      alert("EEE");
+  if(va === 0){
+    document.getElementsByClassName('dropdown-menu')[0].style.visibility = 'visible';
+    btn.parentElement.value = 1;
+    return;
+  }else if(va === 1){
+     document.getElementsByClassName('dropdown-menu')[0].style.visibility = 'hidden';
+     btn.parentElement.value = 0;
+    return;
   }
 }
+
+window.onload = function(){
+    const divToHide = document.getElementsByClassName('dropdown-menu')[0];
+    document.onclick = function(e){
+      if(e.target.className !== 'divToHide'){
+        document.getElementsByClassName('dropdown-menu')[0].style.visibility = 'hidden';
+        btn.parentElement.value = 0;
+      }
+    }
+}
+
 // Get the modal
 // var modal = document.getElementById("myModal");
 
@@ -115,94 +129,94 @@ myAudio.onpause = function () {
 
 
 
-// let gender = 'male';
-// height = 50;
+let gender = 'male';
+height = 50;
 
-// document.querySelector('.calculate .gender .male').onclick = function () {
-//     gender = 'male';
-//     this.classList.add('active');
-//     document.querySelector('.calculate . gender .female').className = 'female';
-// }
+document.querySelector('.calculate .gender .male').onclick = function () {
+    gender = 'male';
+    this.classList.add('active');
+    document.querySelector('.calculate . gender .female').className = 'female';
+}
 
-// document.querySelector('.calculate .gender .male').onclick = function () {
-//     gender = 'female';
-//     this.classList.add('active');
-//     document.querySelector('.calculate . gender .male').className = 'male';
-// }
+document.querySelector('.calculate .gender .male').onclick = function () {
+    gender = 'female';
+    this.classList.add('active');
+    document.querySelector('.calculate . gender .male').className = 'male';
+}
 
-// document.querySelector('.calculate .height input').onchange = function () {
-//     height = parseInt(this.value);
-//     document.querySelector('.calculate .height .val span').innerText = height;
-// }
+document.querySelector('.calculate .height input').onchange = function () {
+    height = parseInt(this.value);
+    document.querySelector('.calculate .height .val span').innerText = height;
+}
 
-// document.querySelector('.calculate .height .val i.add').onclick = function () {
-//     height += 1;
-//     height = (height > 220) ? 220 : height;
-//     document.querySelector('.calculate .height .val span').innerText = height;
-//     document.querySelector('.calculate .height input').value = height;
-// }
+document.querySelector('.calculate .height .val i.add').onclick = function () {
+    height += 1;
+    height = (height > 220) ? 220 : height;
+    document.querySelector('.calculate .height .val span').innerText = height;
+    document.querySelector('.calculate .height input').value = height;
+}
 
-// document.querySelector('.calculate .height .val i.sub').onclick = function () {
-//     height -= 1;
-//     height = (height < 50) ? 50 : height;
-//     document.querySelector('.calculate .height .val span').innerText = height;
-//     document.querySelector('.calculate .height input').value = height;
-// }
+document.querySelector('.calculate .height .val i.sub').onclick = function () {
+    height -= 1;
+    height = (height < 50) ? 50 : height;
+    document.querySelector('.calculate .height .val span').innerText = height;
+    document.querySelector('.calculate .height input').value = height;
+}
 
-// document.querySelector('.calculate .weight input').onchange = function () {
-//     weight = parseInt(this.value);
-//     document.querySelector('.calculate .weight .val span').innerText = weight;
-// }
+document.querySelector('.calculate .weight input').onchange = function () {
+    weight = parseInt(this.value);
+    document.querySelector('.calculate .weight .val span').innerText = weight;
+}
 
-// document.querySelector('.calculate .weight .val i.add').onclick = function () {
-//     weight += 1;
-//     weight = (weight > 180) ? 180 : weight;
-//     document.querySelector('.calculate .weight .val span').innerText = weight;
-//     document.querySelector('.calculate .weight input').value = weight;
-// }
+document.querySelector('.calculate .weight .val i.add').onclick = function () {
+    weight += 1;
+    weight = (weight > 180) ? 180 : weight;
+    document.querySelector('.calculate .weight .val span').innerText = weight;
+    document.querySelector('.calculate .weight input').value = weight;
+}
 
-// document.querySelector('.calculate .weight .val i.sub').onclick = function () {
-//     weight -= 1;
-//     weight = (weight < 10) ? 10 : weight;
-//     document.querySelector('.calculate .weight .val span').innerText = weight;
-//     document.querySelector('.calculate .weight input').value = weight;
-// }
+document.querySelector('.calculate .weight .val i.sub').onclick = function () {
+    weight -= 1;
+    weight = (weight < 10) ? 10 : weight;
+    document.querySelector('.calculate .weight .val span').innerText = weight;
+    document.querySelector('.calculate .weight input').value = weight;
+}
 
-// document.querySelector('.calculate .calc').onclick = function () {
-//     let bmi = (weight / Math.pow(height / 100, 2)).toFixed(2);
-//     height = 50;
-//     weight = 10;
-//     document.querySelector('.calculate .weight input').value = weight;
-//     document.querySelector('.calculate .weight .val span').innerText = weight;
-//     document.querySelector('.calculate .height input').value = height;
-//     document.querySelector('.calculate .height .val span').innerText = height;
+document.querySelector('.calculate .calc').onclick = function () {
+    let bmi = (weight / Math.pow(height / 100, 2)).toFixed(2);
+    height = 50;
+    weight = 10;
+    document.querySelector('.calculate .weight input').value = weight;
+    document.querySelector('.calculate .weight .val span').innerText = weight;
+    document.querySelector('.calculate .height input').value = height;
+    document.querySelector('.calculate .height .val span').innerText = height;
 
-//     document.querySelector('.result .bmi .val').innerText = bmi;
+    document.querySelector('.result .bmi .val').innerText = bmi;
 
-//     if (bmi < 18.5) {
-//         document.querySelector('.result .text').innerText = 'YOU ARE UNDERWEIGHT!';
-//         document.querySelector('.result .bmi .val').style.color = '#3f51b5';
-//         document.querySelector('.result .text').style.color = '#3f51b5';
-//     }
-//     else if (bmi > 18.5 && bmi < 25) {
-//         document.querySelector('.result .text').innerText = 'YOU HAVE A HEALTHY WEIGHT!';
-//         document.querySelector('.result .bmi .val').style.color = '#0f4';
-//         document.querySelector('.result .text').style.color = '#0f4';
-//     }
-//     else {
-//         document.querySelector('.result .text').innerText = 'YOU ARE OVERWEIGHT!';
-//         document.querySelector('.result .bmi .val').style.color = '#ffc107';
-//         document.querySelector('.result .text').style.color = '#ffc107';
-//     }
+    if (bmi < 18.5) {
+        document.querySelector('.result .text').innerText = 'YOU ARE UNDERWEIGHT!';
+        document.querySelector('.result .bmi .val').style.color = '#3f51b5';
+        document.querySelector('.result .text').style.color = '#3f51b5';
+    }
+    else if (bmi > 18.5 && bmi < 25) {
+        document.querySelector('.result .text').innerText = 'YOU HAVE A HEALTHY WEIGHT!';
+        document.querySelector('.result .bmi .val').style.color = '#0f4';
+        document.querySelector('.result .text').style.color = '#0f4';
+    }
+    else {
+        document.querySelector('.result .text').innerText = 'YOU ARE OVERWEIGHT!';
+        document.querySelector('.result .bmi .val').style.color = '#ffc107';
+        document.querySelector('.result .text').style.color = '#ffc107';
+    }
 
-//     document.querySelector('.calculate').style.display = 'none';
-//     document.querySelector('.result').style.display = 'flex';
-// }
+    document.querySelector('.calculate').style.display = 'none';
+    document.querySelector('.result').style.display = 'flex';
+}
 
-// document.querySelector('.result .recal').onclick = function () {
-//     document.querySelector('.result').style.display = 'none';
-//     document.querySelector('.calculate').style.display = 'flex';
-// }
+document.querySelector('.result .recal').onclick = function () {
+    document.querySelector('.result').style.display = 'none';
+    document.querySelector('.calculate').style.display = 'flex';
+}
 
 
 // ABOUT SCRIPT ///////////////////////////////
