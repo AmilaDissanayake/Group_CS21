ab5ac2a9aee9e0386c893404af247eeae8d57790
Navod-Shehan <2019cs196@stu.ucsc.lk>
2021-10-09 17:17:44 +0530

    hm

--- root/accountant/myprofile.php
+++ root/accountant/myprofile.php
@@ -1,31 +1,26 @@
-<?php include "includes/check_login.php" ?>
-
 <!DOCTYPE html>
-
-<html lang="en" dir="ltr">
-
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Student Profile Page Design Example</title>
 
-    <link rel="stylesheet" href="css/dashboard.css">
+    <meta name="author" content="Codeconvey" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet"><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css'>
+
+    <!--Only for demo purpose - no need to add.-->
     <link rel="stylesheet" href="css/demo.css" />
-    <link rel="stylesheet" href="css/style.css">
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    
+	    <link rel="stylesheet" href="css/style.css">
 </head>
-
 <body>
-    <?php include "includes/sidebar.php" ?>
-    <section class="home-section">
-        <?php include "includes/header.php" ?>
-    </section>
-
-
-    <div class="ScriptTop">
+		
+<div class="ScriptTop">
     <div class="rt-container">
         <div class="col-rt-4" id="float-right">
-
+ 
             <!-- Ad Here -->
             
         </div>
@@ -39,9 +34,9 @@
 
 <header class="ScriptHeader">
     <div class="rt-container">
-    <div class="col-rt-12">
-        <div class="rt-heading">
-            <h1>Student Profile Page Design Example</h1>
+    	<div class="col-rt-12">
+        	<div class="rt-heading">
+            	<h1>Student Profile Page Design Example</h1>
                 <p>A responsive student profile page design.</p>
             </div>
         </div>
@@ -50,95 +45,84 @@
 
 <section>
     <div class="rt-container">
-        <div class="col-rt-12">
-            <div class="Scriptcontent">
-
+          <div class="col-rt-12">
+              <div class="Scriptcontent">
+              
 <!-- Student Profile -->
 <div class="student-profile py-4">
-<div class="container">
+  <div class="container">
     <div class="row">
-    <div class="col-lg-4">
+      <div class="col-lg-4">
         <div class="card shadow-sm">
-        <div class="card-header bg-transparent text-center">
+          <div class="card-header bg-transparent text-center">
             <img class="profile_img" src="https://source.unsplash.com/600x300/?student" alt="student dp">
             <h3>Ishmam Ahasan Samin</h3>
-        </div>
-        <div class="card-body">
+          </div>
+          <div class="card-body">
             <p class="mb-0"><strong class="pr-1">Student ID:</strong>321000001</p>
             <p class="mb-0"><strong class="pr-1">Class:</strong>4</p>
             <p class="mb-0"><strong class="pr-1">Section:</strong>A</p>
+          </div>
         </div>
-        </div>
-    </div>
-    <div class="col-lg-8">
+      </div>
+      <div class="col-lg-8">
         <div class="card shadow-sm">
-        <div class="card-header bg-transparent border-0">
+          <div class="card-header bg-transparent border-0">
             <h3 class="mb-0"><i class="far fa-clone pr-1"></i>General Information</h3>
-        </div>
-        <div class="card-body pt-0">
+          </div>
+          <div class="card-body pt-0">
             <table class="table table-bordered">
-            <tr>
+              <tr>
                 <th width="30%">Roll</th>
                 <td width="2%">:</td>
                 <td>125</td>
-            </tr>
-            <tr>
+              </tr>
+              <tr>
                 <th width="30%">Academic Year	</th>
                 <td width="2%">:</td>
                 <td>2020</td>
-            </tr>
-            <tr>
+              </tr>
+              <tr>
                 <th width="30%">Gender</th>
                 <td width="2%">:</td>
                 <td>Male</td>
-            </tr>
-            <tr>
+              </tr>
+              <tr>
                 <th width="30%">Religion</th>
                 <td width="2%">:</td>
                 <td>Group</td>
-            <tr>
+              </tr>
+              <tr>
                 <th width="30%">blood</th>
                 <td width="2%">:</td>
                 <td>B+</td>
-            </tr>
+              </tr>
             </table>
+          </div>
         </div>
-        </div>
-        <div style="height: 26px"></div>
+          <div style="height: 26px"></div>
         <div class="card shadow-sm">
-        <div class="card-header bg-transparent border-0">
+          <div class="card-header bg-transparent border-0">
             <h3 class="mb-0"><i class="far fa-clone pr-1"></i>Other Information</h3>
+          </div>
+          <div class="card-body pt-0">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
         </div>
-        <div class="card-body pt-0">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </div>
-        </div>
+      </div>
     </div>
-    </div>
-</div>
+  </div>
 </div>
 <!-- partial -->
-    </div>
+           
+    		</div>
 		</div>
     </div>
 </section>
-
+     
 
 
     <!-- Analytics -->
 
-<script>
-    let sidebar = document.querySelector(".sidebar");
-    let sidebarBtn = document.querySelector(".sidebarBtn");
-    sidebarBtn.onclick = function() {
-        sidebar.classList.toggle("active");
-        if (sidebar.classList.contains("active")) {
-            sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
-        } else
-            sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
-    }
-</script>
-
-</body>
-
+	</body>
 </html>
