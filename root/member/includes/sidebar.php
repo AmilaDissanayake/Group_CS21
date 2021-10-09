0627a6974f253dcf0662bc19eb4b569cdcbbcdbf
= <pamodhamahagamage98@gmail.com>
2021-10-09 14:16:16 +0530

    Membersoptionsadd

--- root/member/includes/sidebar.php
+++ root/member/includes/sidebar.php
@@ -1,3 +1,4 @@
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <div class="sidebar">
     <div class="logo-details">
         <p>PH</p>
@@ -5,54 +6,89 @@
     </div>
     <ul class="nav-links">
         <li>
-            <a href="#" class="active">
+            <a href="./dashboard.php" class="links_name">
                 <i class='bx bx-grid-alt'></i>
                 <span class="links_name">Dashboard</span>
             </a>
         </li>
+        <div calss="divider">
+            <span class="fade-effect1"></span>
+        </div>
+ 
+        <div calss="divider">
+            <span class="fade-effect1"></span>
+        </div>
+        <!-- <style>
+            .active2 {
+                list-style: circle;
+                height: 150px;
+            }
+        </style>
+        <script>
+            $('#utton').click(function() {
+                $(this).addClass("active2");
+            });
+        </script> -->
         <li>
-            <a href="#">
-                <i class='bx bx-box'></i>
-                <span class="links_name">Product</span>
+            <a href="./progress.php">
+                <i class='bx bx-bar-chart-alt-2'></i>
+                <span class="links_name">Progress</span>
             </a>
         </li>
+        
+        <div calss="divider">
+            <span class="fade-effect1"></span>
+        </div>
         <li>
-            <a href="#">
-                <i class='bx bx-list-ul'></i>
-                <span class="links_name">Order list</span>
+            <a href="./schedule.php">
+                <i class='bx bx-dumbbell'></i>
+                <span class="links_name">Schedule</span>
             </a>
         </li>
+
+
+        <span class="fade-effect1"></span>
+        
         <li>
-            <a href="#">
-                <i class='bx bx-pie-chart-alt-2'></i>
-                <span class="links_name">Analytics</span>
+            <a href="./mealplan.php">
+            <i class='bx bxs-food-menu'></i>
+                <span class="links_name">Meal Plan</span>
             </a>
         </li>
+       
+        <div calss="divider">
+            <span class="fade-effect1"></span>
+        </div>
+
         <li>
-            <a href="#">
-                <i class='bx bx-coin-stack'></i>
-                <span class="links_name">Stock</span>
+            <a href="./membership.php">
+                <i class='bx bx-money'></i>
+                <span class="links_name">Membership</span>
             </a>
         </li>
+
+        <span class="fade-effect1"></span>
         <li>
-            <a href="#">
-                <i class='bx bx-book-alt'></i>
-                <span class="links_name">Total order</span>
+            <a href="./booking.php">
+                <i class='bx bx-time'></i>
+                <span class="links_name">Bookings</span>
             </a>
         </li>
+        
+
+        <div calss="divider">
+            <span class="fade-effect1"></span>
+        </div>
         <li>
-            <a href="#">
-                <i class='bx bx-user'></i>
-                <span class="links_name">Team</span>
+            <a href="./profile.php">
+                <i class='bx bx-user-pin'></i>
+                <span class="links_name">Profile</span>
             </a>
         </li>
-        <li>
-            <a href="#">
-                <i class='bx bx-message'></i>
-                <span class="links_name">Messages</span>
-            </a>
-        </li>
-        <li>
+        <div calss="divider">
+            <span class="fade-effect1"></span>
+        </div>
+        <!-- <li>
             <a href="#">
                 <i class='bx bx-heart'></i>
                 <span class="links_name">Favrorites</span>
@@ -63,9 +99,9 @@
                 <i class='bx bx-cog'></i>
                 <span class="links_name">Setting</span>
             </a>
-        </li>
+        </li> -->
         <li class="log_out">
-            <a href="../admin/includes/logout.php">
+            <a href="../member/includes/logout.php">
                 <i class='bx bx-log-out'></i>
                 <span class=" links_name">Log out</span>
             </a>
--- root/member/mealplan.php
+++ root/member/mealplan.php
@@ -26,6 +26,7 @@
     </section>
 
 
+
 </body>
 
 </html>
