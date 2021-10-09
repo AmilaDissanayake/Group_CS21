a551f36b5267e139977393b4f93de9b0827281f3
AmilaDissanayake <2019cs039@stu.ucsc.lk>
2021-10-09 15:28:28 +0530

    jhjvh

---
+++ root/trainer/includes/header.php
@@ -0,0 +1,11 @@
+<nav>
+    <div class="sidebar-button">
+        <i class='bx bx-menu sidebarBtn'></i>
+        <span class="dashboard">Dashboard</span>
+    </div>
+    <div class="profile-details">
+        <span class='admin_name'> Academy is open today </span>
+
+        <i class='bx bx-chevron-down'></i>
+    </div>
+</nav>
