4a429f6e5f564cf21771ad6f605ed198414bd0c2
Navod-Shehan <2019cs196@stu.ucsc.lk>
2021-10-09 14:46:05 +0530

    awl habei

--- root/accountant/pay_history.php
+++ root/accountant/pay_history.php
@@ -1,4 +1,4 @@
-<?php include "includes/check_login.php" ?>
+<!-- <?php include "includes/check_login.php" ?> -->
 
 <!DOCTYPE html>
 
@@ -35,14 +35,10 @@
                     <thead>
                         <tr>
 
-                            <th>Firstname</th>
-                            <th>Lastname</th>
-                            <th>Username</th>
-                            <th>Phone Number</th>
+                            <th>Name</th>
+                            <th>Payment Date</th>
                             <!-- <th>Address</th> -->
-                            <th>Date Joined</th>
-                            <th>Membership Type</th>
-                            <th>Status</th>
+                            <th>Amount</th>
                         </tr>
                     </thead>
                     <tbody id="output">
@@ -59,7 +55,7 @@
                 $("#search").keypress(function() {
                     $.ajax({
                         type: 'POST',
-                        url: 'includes/member-page.php',
+                        url: 'includes/member-page1.php',
                         data: {
                             name: $("#search").val(),
                         },
