61c88ce8b2e4ea60a1b82d1d223bd6c8cc37a245
= <pamodhamahagamage98@gmail.com>
2021-10-16 12:53:51 +0530

    signupavailERRORfix

--- root/member/booking.php
+++ root/member/booking.php
@@ -12,6 +12,7 @@
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.1.0/main.min.js"></script>
 </head>
 
 <body>
@@ -25,9 +26,14 @@
         <div class="dashcover"></div>
         <div class="duhead"></div>
  
-
-           
+        <div class="divider"></div>
+        <div class="uppart">
+            <div class="note"><h2>Booking</h2></div>
+            <div id='calendar' class="calendar"></div>
+        </div>
+        <div class="divider"></div>   
     </section>
+    
     <?php include "includes/footer.php" ?>
 
 
