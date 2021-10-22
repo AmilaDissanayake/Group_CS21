9fd2a9b2eebd63fc80e3b38ffab05740d482a45e
= <pamodhamahagamage98@gmail.com>
2021-10-22 19:24:09 +0530

    timeslotbooking

--- root/member/booking.php
+++ root/member/booking.php
@@ -12,6 +12,7 @@
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.1.0/main.min.css">
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.1.0/main.min.js"></script>
 </head>
 
@@ -31,22 +32,69 @@
             <div class="vdivid"></div>
             <div class="middle">
                 <div class="note"><h2>Booking</h2></div>
-                <div class="bkstatus">
-                        <h1>Availability Checker</h1>
-                        <div class="avail">
-                            <div class="statbx"><i class='bx bxs-message-square-check'></i></div>
-                            <!-- <i class='bx bxs-message-square-x' ></i> -->
-                            <div class="txtvi">
-                                <p>Avilable for booking</p>
+                <div class="divider"></div>
+                <div class="cont">
+                    <div class="left">
+                        <div class="bkstatus">
+                            <h1>Availability Checker</h1>
+                            <div class="avail">
+                                <div class="calendar-input">
+                                    <input type='Date' required>
+                                    <button class="check_btn" type="submit" name="date-submit">CHECK</button>
+                                </div>
+                                <div class="statbx"><i class='bx bxs-message-square-check'></i></div>
+                                <div class="txtvi">
+                                    <p>Avilable for booking</p>
+                                </div>
                             </div>
                         </div>
-                        <div class="check_button"><button class="check_btn" >CHECK</button></div>
-                </div>
-<!-- 
-                <div id="calendar-t" class="calendar-top">
-                    <div id="calendar" class="calendar"></div>
-                </div> -->
-                <div id='calendar' class="calendar"></div>
+                        <div id="calendar-t" class="calendar-top">
+                            <h1>Calendar</h1>
+                            <div id="calendar" class="calendar"></div>
+                        </div>
+                    </div>
+                    <div class="midvdiv"></div>
+                    <div class="right">
+                        <h1>Time slots</h1>
+                        <div class="tslotlist">
+                            <div class="grid-container">
+                                <div class="grid-item"> 6.00 a.m. -  8.00 a.m.</div>
+                                <div class="grid-item"> 8.00 a.m. - 10.00 a.m.</div>
+                                <div class="grid-item">10.00 a.m. - 12.00 a.m.</div>
+                                <div class="grid-item">12.00 a.m. -  2.00 p.m.</div>
+                                <div class="grid-item"> 2.00 p.m. -  4.00 p.m.</div>
+                                <div class="grid-item"> 4.00 p.m. -  6.00 p.m.</div>
+                                <div class="grid-item"> 6.00 p.m. -  8.00 p.m.</div>
+                                <div class="grid-item"> 8.00 p.m. - 10.00 p.m.</div>
+                            </div>
+                            
+                            
+                            <div class="selectslot">
+                                <h1>Booking for a time slot</h1> 
+                                <div class="select__div">
+                                    <p><i class='bx bxs-time'></i>Select Time Slot</p>
+                                    <label>
+                                        <select id="membership" class="form_input" required name="membership_cc">
+                                            <option value="" disabled selected> Prefer time slot </option>
+                                            <option value=1> 6.00 a.m. -  8.00 a.m. </option>
+                                            <option value=2> 8.00 a.m. - 10.00 a.m. </option>
+                                            <option value=3>10.00 a.m. - 12.00 a.m.</option>
+                                            <option value=4>12.00 a.m. -  2.00 p.m.</option>
+                                            <option value=1>12.00 a.m. -  2.00 p.m.</option>
+                                            <option value=2> 2.00 p.m. -  4.00 p.m.</option>
+                                            <option value=3> 4.00 p.m. -  6.00 p.m.</option>
+                                            <option value=4> 6.00 p.m. -  8.00 p.m.</option>
+                                            <option value=4> 8.00 p.m. - 10.00 p.m.</option>
+                                        </select>
+                                    </label>  
+                                </div>
+                                <div class="fixT">
+                                    <button class="check_btn" id="tbook" type="submit" name="time-submit">MAKE BOOKING</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
             </div>
             <div class="vdivid"></div>
         </div>
