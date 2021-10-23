11d5c4e06a3e58958b6156fee70b0ab41226cb20
Bimsara Kulasekara <bkdeveloping@gmail.com>
2021-10-23 12:26:43 +0530

    dfbfdb

--- root/admin/includes/header.php
+++ root/admin/includes/header.php
@@ -6,6 +6,9 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.js"></script>
 <script type="text/javascript" src="../admin/includes/dist/js/pignose.calendar.full.min.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 
 <nav>
     <div class="sidebar-button">
--- root/admin/payments.php
+++ root/admin/payments.php
@@ -1,5 +1,3 @@
-<?php include "includes/check_login.php" ?>
-
 <!DOCTYPE html>
 
 <html lang="en" dir="ltr">
@@ -71,7 +69,7 @@
 
 
 
-                <div class="add_button"><button class="add_btn" onclick="location.href='add-member.php'">Add Member</button></div>
+                <div class="add_button"><button class="add_btn" onclick="location.href='add-member.php'">Update</button></div>
 
             </div>
 
@@ -105,7 +103,7 @@
                                 $member_result = mysqli_query($conn, $member_query);
                                 $member_row = mysqli_fetch_assoc($member_result);
 
-                                $trainer_query = "SELECT username, image FROM trainer WHERE trainer_id = $row[member_id];";
+                                $trainer_query = "SELECT username, image FROM trainer WHERE trainer_id = $row[trainer_id];";
                                 $trainer_result = mysqli_query($conn, $trainer_query);
                                 $trainer_row = mysqli_fetch_assoc($trainer_result);
 
